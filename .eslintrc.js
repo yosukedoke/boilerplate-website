3bafd7fa38668b801c59d231b8cbfb234eaecc7b
yosukedoke <yosukedoke@ydniku.local>
2020-10-01 14:47:26 +0900

    prettierを導入

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+    'prettier/@typescript-eslint',
   ],
   rules: {
   },
